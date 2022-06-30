b52a9cfb7be3a6977f94e0fdac14f50d8415b559
Kristy <kristy.lau@uwaterloo.ca>
2022-06-30 01:06:39 -0400

    Added involvement text

---
+++ src/components/Involvement/Involvement.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './involvement.scss';
+
+function Involvement() {
+  return(
+    <div className="involvement">
+      <img src="image.png" alt=""/>
+      <div>
+        <h1 className="involvementHeading">
+          Stay Updated and Informed
+        </h1>
+        <p className="involvementBody">
+          It is important to be engaged with the community.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Involvement;

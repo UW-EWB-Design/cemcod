84d69a2deccca02510e510f73dbf15a3a1ba8e3b
Kristy <kristy.lau@uwaterloo.ca>
2022-06-30 01:54:13 -0400

    styled text

--- src/components/Involvement/Involvement.jsx
+++ src/components/Involvement/Involvement.jsx
@@ -3,15 +3,19 @@ import './involvement.scss';
 
 function Involvement() {
   return(
-    <div className="involvement">
-      <img src="image.png" alt=""/>
-      <div>
-        <h1 className="involvementHeading">
+    <div className='involvement'>
+      <img className='involvementImage' src={require('./image.png')} alt=''/>
+      <div className='involvementText'>
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css' />
+        <h1>
           Stay Updated and Informed
         </h1>
-        <p className="involvementBody">
+        <p>
           It is important to be engaged with the community.
         </p>
+        <button className='involvementButton'>
+          GET INVOLVED
+        </button>
       </div>
     </div>
   );

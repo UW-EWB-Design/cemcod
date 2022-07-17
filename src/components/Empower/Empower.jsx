ec19c4855fc9d8bbedfe8ac7838fd1c53043053a
Kristy <kristy.lau@uwaterloo.ca>
2022-07-17 10:24:57 -0400

    Empower the Community Section of Home Page

---
+++ src/components/Empower/Empower.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./empower.scss";
+
+function Empower() {
+  return (
+    <div className="empower">
+      <div className="empowerText">
+        <link
+          href="//fonts.googleapis.com/css?family=Oswald"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <h1>Empower The Community</h1>
+      </div>
+      <div className="empowerButton">
+        <link
+          href="http://fonts.googleapis.com/css?family=Lato:400,700"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <button>HEAR PEOPLE'S STORIES</button>
+      </div>
+    </div>
+  );
+}
+
+export default Empower;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Involvement from "../../components/Involvement/Involvement";
+import Empower from "../../components/Empower/Empower";
 
 function Home() {
   return (
     <div className="home">
       <Involvement />
+      <Empower />
     </div>
   );
 }
